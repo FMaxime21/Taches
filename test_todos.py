22dec1e533bab8fe4f3bf632d87d6f6fe2dc99d9
Shad <65975514+FMaxime21@users.noreply.github.com>
2021-03-20 15:02:37 +0100

    Ajout change_statut

--- test_todos.py
+++ test_todos.py
@@ -2,7 +2,7 @@ from todos import TaskManager, Action_add
 
 
 def test_can_create_an_action():
-    action = Action_add("add", "first task", 1)
+    action = Action_add("add", "first task", 1, "à faire")
     assert action.name == "add"
     assert action.description == "first task"
     assert action.numero == 1
@@ -20,18 +20,43 @@ def test_task_manager_starts_with_no_tasks():
 def test_parse_del():
     command = "- numero"
     numero = 1
+    statut = "à faire"
     task_manager = TaskManager()
 
-    action = task_manager.parse_del(command, numero)
+    action = task_manager.parse_del(command, numero, statut)
 
     assert action.numero == 1
-    
+
 def test_parse_add():
     command = "+ first task"
     numero = 1
+    statut = "à faire"
     task_manager = TaskManager()
 
-    action = task_manager.parse_add(command,numero)
+    action = task_manager.parse_add(command,numero, statut)
 
     assert action.name == "add"
     assert action.description == "first task"
+    assert action.statut == "à faire"
+
+def test_parse_statut_x():
+    command = "x numero"
+    numero = 1
+    statut = "à faire"
+    task_manager = TaskManager()
+
+    action = task_manager.parse_statut_x(command, numero, statut)
+
+    assert action.numero == 1
+    assert action.statut == "à faire"
+
+def test_parse_statut_o():
+    command = "x numero"
+    numero = 1
+    statut = "fait"
+    task_manager = TaskManager()
+
+    action = task_manager.parse_statut_o(command, numero, statut)
+
+    assert action.numero == 1
+    assert action.statut == "fait"
--- todos.py
+++ todos.py
@@ -2,25 +2,33 @@ class TaskManager:
     def __init__(self):
         self.tasks = []
 
-    def parse_add(self, command, numero):
-        return Action_add("add", command[2:], 1) 
+    def parse_add(self, command, numero, statut):
+        return Action_add("add", command[2:], 1, "à faire") 
 
-    def parse_del(self, command, numero):
-        return Action_del("del", command[2:], 1)
+    def parse_del(self, command, numero, statut):
+        return Action_del("del", command[2:], 1, "à faire")
+
+    def parse_statut_x(self,command, numero, statut):
+        return Action_add("del", command[2:], 1, "à faire")
+
+    def parse_statut_o(self,command, numero, statut):
+        return Action_add("del", command[2:], 1, "fait")
 
 class Action_add:
-    def __init__(self, name, description, numero):
+    def __init__(self, name, description, numero, statut):
         self.name = name
         self.description = description
         self.numero = numero
+        self.statut = statut
 
     def __del__(self):
         del self   
 
 class Action_del:
-    def __init__(self, name, description, numero):
+    def __init__(self, name, description, numero, statut):
         self.name = name
         self.description = description
         self.numero = numero
-        del self            
+        self.statut = statut
+        del self
         
