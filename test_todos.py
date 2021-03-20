dc4d53eb3e186b8f20c63210847879a14ede8884
Shad <65975514+FMaxime21@users.noreply.github.com>
2021-03-20 14:31:54 +0100

    Ajout delete tache

--- test_todos.py
+++ test_todos.py
@@ -1,10 +1,11 @@
-from todos import TaskManager, Action
+from todos import TaskManager, Action_add
 
 
 def test_can_create_an_action():
-    action = Action("add", "first task")
+    action = Action_add("add", "first task", 1)
     assert action.name == "add"
     assert action.description == "first task"
+    assert action.numero == 1
 
 
 def test_can_create_a_task_manager():
@@ -16,12 +17,21 @@ def test_task_manager_starts_with_no_tasks():
 
     assert task_manager.tasks == []
 
-
-def test_parse_add():
-    command = "+ first task"
+def test_parse_del():
+    command = "- numero"
+    numero = 1
     task_manager = TaskManager()
 
-    action = task_manager.parse(command)
+    action = task_manager.parse_del(command, numero)
+
+    assert action.numero == 1
+    
+def test_parse_add():
+    command = "+ first task"
+    numero = 1
+    task_manager = TaskManager()
+
+    action = task_manager.parse_add(command,numero)
 
     assert action.name == "add"
     assert action.description == "first task"
--- todos.py
+++ todos.py
@@ -1,12 +1,26 @@
 class TaskManager:
     def __init__(self):
         self.tasks = []
-        
-    def parse(self, command):
-        return Action("add", command[2:])
 
+    def parse_add(self, command, numero):
+        return Action_add("add", command[2:], 1) 
 
-class Action:
-    def __init__(self, name, description):
+    def parse_del(self, command, numero):
+        return Action_del("del", command[2:], 1)
+
+class Action_add:
+    def __init__(self, name, description, numero):
         self.name = name
         self.description = description
+        self.numero = numero
+
+    def __del__(self):
+        del self   
+
+class Action_del:
+    def __init__(self, name, description, numero):
+        self.name = name
+        self.description = description
+        self.numero = numero
+        del self            
+        
